893727f2c7aa310fe10ecce165689de11528c956
Emily Pearl <Emilypearl9193@gmail.com>
2023-12-07 13:23:24 -0700

    changed hover on navbar

--- src/lib/nav/Navbar.svelte
+++ src/lib/nav/Navbar.svelte
@@ -1,8 +1,8 @@
 <nav class='h-20 bg-sbb'>
   <ul class='flex flex-row flex justify-end'>
-    <li class='p-2 m-3 text-stl text-xl h-12 hover:bg-stl hover:text-personality rounded'><a href='/'>Home</a></li>
-    <li class='p-2 m-3 text-stl text-xl h-12 hover:bg-stl hover:text-personality rounded'><a href='/library'>Library</a></li>
-    <li class='p-2 m-3 text-stl text-xl h-12 hover:bg-stl hover:text-personality rounded'><a href='/goals'>Goals</a></li>
-    <li class='p-2 m-3 text-stl text-xl h-12 hover:bg-stl hover:text-personality rounded'><a href='/journal'>Journal</a></li>
+    <li class='p-2 m-3 text-stl text-xl h-12 hover:bg-sga rounded'><a href='/'>Home</a></li>
+    <li class='p-2 m-3 text-stl text-xl h-12 hover:bg-sga rounded'><a href='/library'>Library</a></li>
+    <li class='p-2 m-3 text-stl text-xl h-12 hover:bg-sga rounded'><a href='/goals'>Goals</a></li>
+    <li class='p-2 m-3 text-stl text-xl h-12 hover:bg-sga rounded'><a href='/journal'>Journal</a></li>
   </ul>
 </nav>
---
+++ src/routes/library/+page.svelte
@@ -0,0 +1,19 @@
+<body>
+  
+
+  <section class="grid-cols-12 grid-rows-6 grid">
+    <h1 class='text-5xl m-10 text-sbb col-span-2'>Recents</h1>
+  
+    <h1 class='text-5xl m-10 text-sbb row-start-4 col-span-2'>Fantasy</h1>
+  
+  
+  <h2 class="text-sbb text-2xl mt-10 col-start-10 col-end-12 ">your goal progress</h2>
+  </section>
+  </body>
+  
+  
+  <style>
+    :global(body) {
+      background-color: #393E41;
+    }
+  </style>
