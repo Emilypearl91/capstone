27d766e450be2d9d83716e1632552fac9730a0ec
Emily Pearl <Emilypearl9193@gmail.com>
2023-12-07 12:59:11 -0700

    svelte project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        sbb: '#F0EEE0',
+        personality: '#DDB7AB',
+        sga: '#999B84',
+        stl: '#393E41',
+      },
+    }
+  },
+  plugins: []
+};
+
